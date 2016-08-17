72ffda53130dc7133cf0617d5c3113e840abb26c
andrewerrico <aserrico@gmail.com>
2016-08-17 08:34:35 -0400

    added a global close button, created new sliding functions

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,33 +1,59 @@
 ( function() {
 
-  var menuContainer, menu, menuClose;
+  var menuContainer, menu, menuClose, menuCount;
+
 
   menuSlideLeft = document.getElementById('slideLeft');
   menuSlideRight = document.getElementById('slideRight');
-  menuClose = document.getElementsByClassName('close-menu')[0];
+  menuSlideTop = document.getElementById('slideTop');
+  menuSlideBottom = document.getElementById('slideBottom');
   menuContainer = document.getElementsByClassName('main-menu');
+  menuCount = menuContainer.length;
 
-  // Sliding
+  // Sliding Triggers
   var slideFromLeft = document.getElementById('slideFromLeft');
   var slideFromRight = document.getElementById('slideFromRight');
+  var slideFromTop = document.getElementById('slideFromTop');
+  var slideFromBottom = document.getElementById('slideFromBottom');
 
   slideFromLeft.onclick = function() {
-    if ( -1 !== menuSlideLeft.className.indexOf(' menu-open') ) {
-      menuSlideLeft.className = menuSlideLeft.className.replace(' menu-open', '');
-    } else {
+    if ( -1 == menuSlideLeft.className.indexOf(' menu-open') ) {
+      closeMenu();
       menuSlideLeft.className+=" menu-open"
     }
   }
   slideFromRight.onclick = function() {
-    if ( -1 !== menuSlideRight.className.indexOf('slide-from-right menu-open') ) {
-      menuSlideRight.className = menuSlideRight.className.replace(' slide-from-right menu-open', '');
-    } else {
+    if ( -1 == menuSlideRight.className.indexOf('slide-from-right menu-open') ) {
+      closeMenu();
       menuSlideRight.className+=" slide-from-right menu-open"
     }
+  }
 
+  slideFromTop.onclick = function() {
+    if ( -1 == menuSlideTop.className.indexOf('slide-from-top menu-open') ) {
+      closeMenu();
+      menuSlideTop.className+=" slide-from-top menu-open"
+    }
   }
-  menuClose.onclick = function() {
-    console.log('close')
-    menuContainer.className = menuContainer.className.replace(' menu-open', '');
+
+  slideFromBottom.onclick = function() {
+    if ( -1 == menuSlideBottom.className.indexOf('slide-from-bottom menu-open') ) {
+      closeMenu();
+      menuSlideBottom.className+=" slide-from-bottom menu-open"
+    }
   }
+
+  closeMenu = function() {
+    for ( var i = 0; i < menuCount; i++ ) {
+      /*
+      if (menuContainer[i].classList.contains('menu-open')) {
+        menuContainer[i].classList.remove('menu-open');
+      }
+      */
+      if ( -1 !== menuContainer[i].className.indexOf(' menu-open') ) {
+        menuContainer[i].className = menuContainer[i].className.replace(' menu-open', '');
+      }
+    }
+  }
+
 }) ();
